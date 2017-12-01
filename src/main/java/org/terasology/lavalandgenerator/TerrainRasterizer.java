6da01d1efbfccd5ee1762ced03aa490e7fd52662
ContronThePanda <rajtel99@gmail.com>
2017-11-30 22:09:53 -0500

    World now generates in layers of regular stone, hard stone, and mantle stone

--- src/main/java/org/terasology/lavalandgenerator/TerrainRasterizer.java
+++ src/main/java/org/terasology/lavalandgenerator/TerrainRasterizer.java
@@ -10,11 +10,13 @@ import org.terasology.world.generation.WorldRasterizer;
 import org.terasology.world.generation.facets.SurfaceHeightFacet;
 
 public class TerrainRasterizer implements WorldRasterizer {
-    Block stone;
+    Block stone, hardStone, mantleStone;
     
     @Override
     public void initialize() {
         stone = CoreRegistry.get(BlockManager.class).getBlock("Core:Stone");
+        hardStone = CoreRegistry.get(BlockManager.class).getBlock("Core:HardStone");
+        mantleStone = CoreRegistry.get(BlockManager.class).getBlock("Core:MantleStone");
     }
 
     @Override
@@ -22,7 +24,13 @@ public class TerrainRasterizer implements WorldRasterizer {
         SurfaceHeightFacet surfaceHeightFacet = chunkRegion.getFacet(SurfaceHeightFacet.class);
         for(Vector3i position : chunkRegion.getRegion()) {
             float surfaceHeight = surfaceHeightFacet.getWorld(position.x,position.z);
-            if(position.y < surfaceHeight) {
+            if(position.y < 3 && position.y < surfaceHeight) {//Bottom of the world
+                chunk.setBlock(ChunkMath.calcBlockPos(position),mantleStone);
+            }
+            if(surfaceHeight - position.y > 5) {    //Beneath Surface
+                chunk.setBlock(ChunkMath.calcBlockPos(position),hardStone);
+            }
+            else if(position.y < surfaceHeight) {   //Surface
                 chunk.setBlock(ChunkMath.calcBlockPos(position),stone);
             }
         }
